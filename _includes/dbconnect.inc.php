c8a79161cccda4ff1cc1872dab34cecb2a956064
bknapp <knappbrandon462@gmail.com>
2013-06-02 13:23:28 -0400

    First upload containing file folders and files

---
+++ _includes/dbconnect.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+	//HTDOCS
+	//$db = mysqli_connect("localhost", "route_user", "route", "routes") or die("Cannot open Database");
+	
+	//TOTALCHOICE
+	$db = mysqli_connect("localhost", "vymmydwg_route", "routes", "vymmydwg_routes") or die("Cannot open Database");
+?>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Kms</title>
+	<!-- Main CSS Style -->
+	<link rel="stylesheet" type="text/css" href="_css/global.css" />
+	<link rel="stylesheet" type="text/css" href="_css/navigation.css" />
+	<!-- Navigations CSS -->
+	<link rel="stylesheet" type="text/css" href="" />
+	<!-- Javascript area -->
+	<script src="_js/modernizr.custom.js"></script>
+</head>
+
+<body id="wrapper">
+	<div class="leftpage">
+		<!-- Logo text -->
+		<h1>KNAPP</h1>
+		<h2>Managment System</h2>
+		<!-- Admin navigation -->
+		<ul id="navigation">
+			<li><a href="#">Posts</a></li>
+			<li><a href="#">Photo</a></li>
+			<li><a href="#">Video</a></li>
+			<li><a href="#">Users</a></li>
+			<li><a href="#">Logout</a></li>
+		</ul>
+		<!-- End Admin navigation -->
+	</div>
+	<script src="_js/ytmenu.js"></script>
+</body>
+
+</html>
